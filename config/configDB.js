7cd9f17558202c380e45c6eda2a71a606bd59fa2
AishwaryaSawant <sawant.ais@northeastern.edu>
2022-04-05 14:31:32 -0400

    test

---
+++ config/configDB.js
@@ -0,0 +1,20 @@
+const env = process.env;
+
+const config = {
+    db: {
+        host: env.DB_HOST,
+        user: env.DB_USER,
+        password: env.DB_PASSWORD,
+        database: env.DB_NAME,
+        dialect: "mysql",
+        port:3306,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+};
+
+module.exports = config;
---
+++ scripts/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#start app
+cd /home/ec2-user/webservice
+pm2 kill
+sudo npm i
+sleep 30
+sudo pm2 start index.js 
+sudo pm2 save
+sudo pm2 startup systemd
+sudo pm2 restart all --update-env
